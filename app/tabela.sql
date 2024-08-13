fe89a82b228f0379973059365e8cd2119bfdbf19
Letícia Alves <leticiadeoliveira.corp@gmail.com>
2024-08-12 23:59:48 -0300

    alterando credenciais de acesso ao banco

--- app/tabela.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE membros
-(
-    id        INT             NOT NULL    AUTO_INCREMENT,
-    nome      VARCHAR(350)    NOT NULL,
-    email     VARCHAR(150)    NOT NULL,  
-    telefone     VARCHAR(150)    NOT NULL,
-    cpf     VARCHAR(150)    NOT NULL,          
-    PRIMARY KEY (`id`)
-) ENGINE = InnoDB;
